602ac104a4cb16c18ff571f71fa0febe336c97b0
TanujaP07 <tanuja.pyneni7898@gmail.com>
2023-01-23 18:45:10 -0600

    header section created

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import User from "./User";
+
+export default function Header(){
+    return(
+        <header className = "flex justify-between p-5 text-sm text-gray-700">
+            {/*layered components from global.css : headiv, link*/}
+            <div className = "headiv"> 
+                <p className = "link">About</p>
+                <p className = "link">Store</p>
+            </div>
+            <div className = "headiv"> 
+                <p className = "link">Gmail</p>
+                <p className = "link">Images</p>
+                <User/>
+            </div>
+        </header>
+    )
+}
