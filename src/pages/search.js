1232825240a9b360f6214600284bf67a41f8bafa
TanujaP07 <tanuja.pyneni7898@gmail.com>
2023-01-25 23:43:11 -0600

    Created the about data section and modified the search page title

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import SearchHeader from "components/SearchHeader";
 import { useRouter } from "next/router";
+import SearchResults from "components/SearchResults";
 
 export default function search({results}){
     console.log(results);
@@ -14,17 +15,19 @@ export default function search({results}){
             <SearchHeader/>
 
             {/*Search Results*/}
+            <SearchResults results={results}/>
         </div>
     );
 }
 
 export async function getServerSideProps(context){
-    const mockData = false
+    const startIndex = context.query.start || "1";
+    const mockData = true
     const data = mockData ? Response 
     : await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}`
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}&start=${startIndex}`
     ).then((response)=>response.json());
-    return{
+    return {
         props:{
             results: data,
         },
